615b5222773af09c05d79f7f977d0f503540593b
Grigoriy <gromanenko98@gmail.com>
2021-04-28 17:36:26 +0700

    original version

---
+++ src/main/java/se/kth/ict/id2203/components/epfd/Epfd.java
@@ -0,0 +1,14 @@
+package se.kth.ict.id2203.components.epfd;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import se.sics.kompics.ComponentDefinition;
+
+public class Epfd extends ComponentDefinition {
+
+	private static final Logger logger = LoggerFactory.getLogger(Epfd.class);
+
+	public Epfd(EpfdInit init) {
+	}
+}
